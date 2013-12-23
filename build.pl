c452b0bad04836f96ccdf9e4b77ef1c7f164fca3
dm47021 <dm47021@hotmail.com>
2013-12-23 18:49:35 -0500

    add build script and kernel packer

---
+++ build.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -W
+use strict;
+use Cwd;
+
+my $dir = getcwd;
+
+print "\ncleaning kernel source\n";
+
+
+print "\nremoving old boot.img\n";
+system ("rm boot.img");
+system ("rm $dir/zpack/zcwmfiles/boot.img");
+
+print "\nremoving old Evodesign.zip\n";
+system ("rm $dir/Evodesign.zip");
+
+print "\ncreating ramdisk from Evodesign folder\n";
+chdir ("$dir/zpack");
+
+ unless (-d "$dir/zpack/Evodesign/data") {
+ system ("mkdir Evodesign | tar -C /$dir/zpack/Evodesign/ -xvf Evodesign.tar");
+ }
+
+chdir ("$dir/zpack/Evodesign");
+system ("find . | cpio -o -H newc | gzip > $dir/ramdisk-repack.gz");
+
+
+print "\nbuilding zImage from source\n";
+chdir ("$dir");
+system ("cp defconfig $dir/.config");
+system ("make -j8");
+
+print "\ncreating boot.img\n";
+chdir $dir or die "/zpack/Evodesign $!";;
+system ("$dir/zpack/mkbootimg --cmdline '' --kernel $dir/arch/arm/boot/zImage --ramdisk ramdisk-repack.gz -o boot.img --base 0x05000000 --pagesize 4096");
+
+unlink("ramdisk-repack.gz") or die $!;
+
+print "\ncreating flashable zip file\n";
+system ("cp drivers/net/wireless/bcm4329/bcm4329.ko $dir/zpack/zcwmfiles/system/lib/modules/");
+system ("cp boot.img $dir/zpack/zcwmfiles/");
+chdir ("$dir/zpack/zcwmfiles");
+system ("zip -9 -r $dir/Evodesign.zip *");
+print "\nceated Evodesign.zip\n";
+
+print "\nremoving Evodesign.zip from sdcard\n";
+system ("adb shell rm /sdcard/Evodesign.zip");
+
+print "\npushing new Evodesign.zip to sdcard\n";
+system ("adb push $dir/a200.zip /sdcard/Evodesign.zip");
+print "\ndone\n";
